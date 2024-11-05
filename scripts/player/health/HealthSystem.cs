78c9192a1c29ce3799d7ef50de5cd86045022a60
PolarDyth <williamalexander1603@gmail.com>
2024-11-05 17:31:19 +0000

    Added basic health system
    Still to be tested and sprites to be done correctly.

---
+++ scripts/player/health/HealthSystem.cs
@@ -0,0 +1,78 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class HealthSystem : Node
+{
+
+
+	// Signals
+	[Signal]
+	public delegate void maxHealthChangedEventHandler(int maxHealth);
+	[Signal]
+	public delegate void HealthChangedEventHandler(int health);
+	[Signal]
+	public delegate void HealthDepletedEventHandler();
+
+	// Backing fields
+	private int _health = 0; // Default value for health
+	private int _maxHealth = 20; // Default value for maxHealth
+	private bool _invincible = false; // Default value for invincible
+
+	// Properties
+
+	[Export]
+	public int Health
+	{
+		get 
+		{
+			return _health;
+		}
+		set
+		{
+			_health = value;
+			EmitSignal("HealthChangedEventManager", _health);
+
+			if (_health <= 0)
+			{
+				EmitSignal("HealthDepletedEventManager");
+			}
+		}
+	}
+
+	[Export]
+	private int MaxHealth
+	{
+		get 
+		{
+			return _maxHealth;
+		}
+		set
+		{ 
+			if (value < 0)
+			{
+				_maxHealth = 0;
+			}
+			else
+			{
+				_maxHealth = value;
+			}
+		}
+	}
+
+	[Export]
+	private bool Invincible
+	{ 
+		get { return _invincible; }
+		set { _invincible = value; }
+	}
+
+	private Timer immuneTimer = new Timer();
+
+	// Methods
+
+	public void _ready()
+	{
+
+	}
+}
